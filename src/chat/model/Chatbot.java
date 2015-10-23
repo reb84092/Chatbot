e7c1f013180d9aa39a0e71dc4793236098bd3fae
reb84092 <rbar3155@740s-pl-102.local>
2015-10-23 09:32:48 -0600

    made it more like it should be.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -3,14 +3,27 @@ package chat.model;
 import java.util.ArrayList;
 
 /**
- * Base version of the Chatbot class. Only stub methods are provided. Students will complete method as part of the project.
+ * Base version of the Chatbot class. Only stub methods are provided. Students will complete method as part 
+ * of the project.
  * @author rbar3155
  * @version 1.2 10/23/15 Completed the lengthChecker method. Fixed the Constructor and getter for userName
  */
+public class Chatbot
 {
-
-	public String getUserName()
+	private ArrayList<String> memesList;
+	private ArrayList<String> politicalTopicList;
+	private String userName;
+	private String content;
+	
+	/** Creates an instance of the Chatbot with the supplied username.*/
+	public Chatbot(String userName)
 	{
-		
+		this.userName = userName;
 	}
-}
+	
+	private void buildMemesList()
+	
+	private void buildPoliticalTopicsList()
+	
+	/** Checks the length of the supplied string.*/
+	public boolean lengthChecke(String currentInput)
