3b1d758f3357f6ccc15d5ff49deaf0f296b8f612
reb84092 <reb84092@gmail.com>
2016-03-07 09:14:57 -0700

    Added method to send a tweet

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,12 +1,31 @@
 package chat.model;
 
+import twitter4j.*;
+
 import java.util.ArrayList;
 
 /**
  * @RachelBarnes
- * @1.0 First part of Twitter project
+ * @1.1 First part of Twitter project
  */
-public class CTECTwitter 
-{
+public class CTECTwitter {
 
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
+
+	public CTECTwitter() {
+		statusList = new ArrayList<Status>();
+	}
+
+	public void sendTweet(String tweet) 
+	{
+		try {
+			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
