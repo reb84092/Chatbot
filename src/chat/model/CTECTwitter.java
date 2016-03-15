db943a27f0540d02417f5c163b6008ac688895f5
reb84092 <reb84092@gmail.com>
2016-03-15 08:27:50 -0600

    Added remove text method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -9,7 +9,8 @@ import java.util.List;
  * @RachelBarnes
  * @1.1 First part of Twitter project
  */
-public class CTECTwitter {
+public class CTECTwitter
+{
 
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
@@ -17,12 +18,14 @@ public class CTECTwitter {
 	private ArrayList<String> tweetTexts;
 	private ChatController baseController;
 
-	public CTECTwitter(ChatController baseController) {
+	public CTECTwitter(ChatController baseController)
+	{
 		this.baseController = baseController;
 		statusList = new ArrayList<Status>();
 		chatbotTwitter = TwitterFactory.getSingleton();
 		tweetTexts = new ArrayList<String>();
 	}
+
 	public void loadTweets(String twitterHandle) throws TwitterException
 	{
 		Paging statusPage = new Paging(1, 200);
@@ -33,7 +36,7 @@ public class CTECTwitter {
 			statusList.addAll(chatbotTwitter.getUserTimeline(twitterHandle, statusPage));
 			page++;
 		}
-		for (Status currentStatus: statusList)
+		for (Status currentStatus : statusList)
 		{
 			String[] tweetText = currentStatus.getText().split(" ");
 			for (String word : tweetText)
@@ -43,23 +46,30 @@ public class CTECTwitter {
 		}
 		removeCommonEnglishWords(tweetTexts);
 		removeEmptyText();
-		
+
 	}
-	
+
 	private void removeEmptyText()
 	{
-		
+		for (int spot = 0; spot < tweetTexts.size(); spot++)
+		{
+			if (tweetTexts.get(spot).equals(""))
+			{
+				tweetTexts.remove(spot);
+				spot--;
+			}
+		}
 	}
-	
+
 	private List removeCommonEnglishWords(List<String> wordList)
 	{
 		return null;
 	}
-	
-	private String removePunctuation(String currentString)
+
+	private String removePunctuation(String currentString) //Goes through the characters and returns everything that isn't in the punctuation list.
 	{
 		String punctuation = ".,'?!:;\"(){}^[]<>-";
-		
+
 		String scrubbedString = "";
 		for (int i = 0; i < currentString.length(); i++)
 		{
@@ -70,14 +80,14 @@ public class CTECTwitter {
 		}
 		return scrubbedString;
 	}
-	
-	public void sendTweet(String tweet) {
-		try 
+
+	public void sendTweet(String tweet)
+	{
+		try
 		{
-			chatbotTwitter
-					.updateStatus("Rachel just tweeted from my Java Chatbot" + "program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
-		} 
-		catch (TwitterException error) 
+			chatbotTwitter.updateStatus("Rachel just tweeted from my Java Chatbot" + "program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		}
+		catch (TwitterException error)
 		{
 
 			baseController.handleErrors(error.getErrorMessage());
